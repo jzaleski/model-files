92a7469de21496a832afd104b0fb35ea9e610ab1
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2026-02-19 13:08:05 -0500

    feat: Add "MODEL_FAMILY" support

--- bin/run-coder.sh
+++ bin/run-coder.sh
@@ -2,36 +2,44 @@
 
 set -e;
 
-MODEL=${MODEL:-"unsloth/GLM-${MODEL_VERSION:-"4.7-Flash"}-GGUF:Q${QUANT:-5}_K_${PARAMETERS:-"M"}"};
-ALIAS=${ALIAS:-"jzaleski/coder"};
+MODEL_FAMILY=${MODEL_FAMILY:-"GLM"};
+MODEL_FLAGS=();
 
-HOST=${HOST:-"0.0.0.0"};
-PORT=${PORT:-"8081"};
-
-CTX_SIZE=${CTX_SIZE:-"65536"};
-FIT=${FIT:-"on"};
-FLASH_ATTN=${FLASH_ATTN:-"on"};
-MIN_P=${MIN_P:-"0.01"};
-N_GPU_LAYERS=${N_GPU_LAYERS:-"-1"};
-REPEAT_PENALTY=${REPEAT_PENALTY:-"1.0"}
-TEMP=${TEMP:-"0.55"};
-TOP_K=${TOP_K:-"30"};
-TOP_P=${TOP_P:-"0.9"};
+if [ "$MODEL_FAMILY" = "GLM" ]; then
+  MODEL="unsloth/GLM-${MODEL_VERSION:-"4.7-Flash"}-GGUF:Q${QUANT:-5}_K_${PARAMETERS:-"M"}";
+  MODEL_FLAGS+=(
+    "--ctx-size ${CTX_SIZE:-"65536"}"
+    "--min-p ${MIN_P:-"0.01"}"
+    "--n-gpu-layers ${N_GPU_LAYERS:-"99"}"
+    "--repeat-penalty ${REPEAT_PENALTY:-"1.0"}"
+    "--temp ${TEMP:-"0.7"}"
+    "--top-p ${TOP_P:-"0.95"}"
+  );
+elif [ "$MODEL_FAMILY" = "QWEN" ]; then
+  MODEL="unsloth/Qwen3-Coder-${MODEL_VERSION:-"480B-A35B-Instruct"}-GGUF:Q${QUANT:-"2"}_K_${PARAMETERS:-"XL"}"
+  MODEL_FLAGS+=(
+    "--ctx-size ${CTX_SIZE:-"65536"}"
+    "--min-p ${MIN_P:-"0.0"}"
+    "--n-gpu-layers ${N_GPU_LAYERS:-"99"}"
+    "--override-tensor ${OVERRIDE_TENSOR:-".ffn_.*_exps.=CPU"}"
+    "--repeat-penalty ${REPEAT_PENALTY:-"1.05"}"
+    "--temp ${TEMP:-"0.7"}"
+    "--top-k ${TOP_K:-"20"}"
+    "--top-p ${TOP_P:-"0.8"}"
+  );
+else
+  echo "Unmapped model-family: \"$MODEL_FAMILY\"" 1>&2;
+  exit 1;
+fi
 
 llama-server \
   -hf ${MODEL} \
-  --alias ${ALIAS} \
-  --host ${HOST} \
-  --port ${PORT} \
+  --alias ${ALIAS:-"jzaleski/coder"} \
+  --host ${HOST:-"0.0.0.0"} \
+  --port ${PORT:-"8081"} \
   --jinja \
   --kv-unified \
   --mlock \
-  --ctx-size ${CTX_SIZE} \
-  --fit ${FIT} \
-  --flash-attn ${FLASH_ATTN} \
-  --min-p ${MIN_P} \
-  --n-gpu-layers ${N_GPU_LAYERS} \
-  --repeat-penalty ${REPEAT_PENALTY} \
-  --temp ${TEMP} \
-  --top-k ${TOP_K} \
-  --top-p ${TOP_P};
+  --fit ${FIT:-"on"} \
+  --flash-attn ${FLASH_ATTN:-"on"} \
+  "${MODEL_FLAGS[@]}";
