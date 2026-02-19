66761e355902fbd699242ccf9c21b5025ed4a49c
Jonathan W. Zaleski <JonathanZaleski@gmail.com>
2026-02-19 16:55:29 -0500

    feat: Tease apart `coder` & `coder-experimental` scripts

--- bin/run-coder-experimental.sh
+++ bin/run-coder-experimental.sh
@@ -3,8 +3,8 @@
 set -e;
 
 llama-server \
-  -hf "unsloth/GLM-${MODEL_VERSION:-"4.7-Flash"}-GGUF:Q${QUANT:-5}_K_${PARAMETERS:-"M"}" \
-  --alias ${ALIAS:-"jzaleski/coder"} \
+  -hf "unsloth/${MODEL_VERSION:-"Qwen3.5-397B-A17B"}-GGUF:Q${QUANT:-5}_K_${PARAMETERS:-"M"}" \
+  --alias ${ALIAS:-"jzaleski/coder-experimental"} \
   --host ${HOST:-"0.0.0.0"} \
   --port ${PORT:-"8081"} \
   --ctx-size ${CTX_SIZE:-"32768"} \
@@ -12,10 +12,11 @@ llama-server \
   --flash-attn ${FLASH_ATTN:-"on"} \
   --jinja \
   --kv-unified \
-  --min-p ${MIN_P:-"0.01"} \
+  --min-p ${MIN_P:-"0.0"} \
   --mlock \
   --n-gpu-layers ${N_GPU_LAYERS:-"-1"} \
   --repeat-penalty ${REPEAT_PENALTY:-"1.0"} \
-  --temp ${TEMP:-"0.7"} \
+  --temp ${TEMP:-"0.6"} \
   --threads ${THREADS:-"32"} \
+  --top-k ${TOP_K:-"20"} \
   --top-p ${TOP_P:-"0.95"};
